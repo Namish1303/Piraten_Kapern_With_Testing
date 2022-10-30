e63b6f9eb6d251949a78844695cea85f01bb9be3
Namish Arora <namish.arora@gmail.com>
2022-10-30 11:52:37 -0400

    A-Test
    Row 78
    Sorceress
    Scored 1000 pts

--- src/test/java/AcceptancePart2.java
+++ src/test/java/AcceptancePart2.java
@@ -59,5 +59,40 @@ public class AcceptancePart2 {
 
     }
 
+    @Test
+    public void Row78()
+    {
+        d = g.getGameDices();
+        System.out.println("ROW 78: 3 skulls, 3 parrots, 2 swords ");
+        Dice skull = new Dice("Skull");
+        Dice sword = new Dice("Sword");
+        Dice parrot  = new Dice("Parrot");
+
+        c = new Card("Sorceress",1,0);
+
+        d[0]=d[1]=d[2]= skull;
+        d[3]=d[4]=d[5] = parrot;
+        d[6]=d[7] = sword;
+
+        System.out.println("        To check if skull can be rolled then reroll");
+        int[] pos2 ={3};
+        Assertions.assertEquals(2,g.isMoveValid(d,pos2,c));
+
+        if(g.isMoveValid(d,pos2,c) == 2 )
+        {
+            c = new Card("null",1,0);
+            d = g.shuffleDice(d,pos2);
+            d[2] = parrot;
+        }
+
+        int[] pos = {7,8};
+        System.out.println("        Re-rolling 2 swords");
+        d = g.shuffleDice(d,pos);
+        d[6]=d[7] = parrot;
+
+        Assertions.assertEquals(1000,g.regularPts(d,c));
+    }
+
+
 
 }
