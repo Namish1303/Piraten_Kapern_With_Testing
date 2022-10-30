a0bb2e01009e0d2f3ef11bce135ea61a581f926e
Namish Arora <namish.arora@gmail.com>
2022-10-29 20:40:23 -0400

    A-Test
    Created Class for acceptance testing

---
+++ src/test/java/AcceptancePart1.java
@@ -0,0 +1,14 @@
+public class AcceptancePart1 {
+    Dice[] d;
+    Card c;
+    Game g;
+    public AcceptancePart1()
+    {
+        Dice[] d = new Dice[8];
+        c= new Card("Coin",1,0);
+        g = new Game();
+    }
+
+    
+
+}
