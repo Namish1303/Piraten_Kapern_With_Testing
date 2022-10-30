792656e4a9a05986ecfad0fbaf706540c5d0e373
Namish Arora <namish.arora@gmail.com>
2022-10-29 20:44:42 -0400

    A-Test
    Row 45
    3 Skulls and 5 Swords, which means the player dies

--- src/test/java/AcceptancePart1.java
+++ src/test/java/AcceptancePart1.java
@@ -1,14 +1,37 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class AcceptancePart1 {
     Dice[] d;
     Card c;
     Game g;
     public AcceptancePart1()
     {
-        Dice[] d = new Dice[8];
+        d = new Dice[8];
         c= new Card("Coin",1,0);
         g = new Game();
     }
 
-    
+    @Test
+    public void Row45()
+    {
+        for(int i=0;i<8;i++)
+        {
+            d[i] = new Dice("Sword");
+        }
+
+        Dice skull = new Dice("Skull");
+        d[0] = skull;
+        d[1] = skull;
+        d[2] = skull;
+
+        // 3 skulls and 5 swords
+
+        System.out.println("\n\nROW 45: 3 skulls and 5 swords = Die");
+        Assertions.assertTrue(g.endTurn(d,c));
+    }
+
+
+
 
 }
